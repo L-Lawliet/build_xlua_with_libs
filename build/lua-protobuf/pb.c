55445b6abbf4952561f8760904387b507bd10a2e
Lawliet <1577075604@qq.com>
2022-08-22 18:44:29 +0800

    重新增加pb的uwp兼容性提示

--- build/lua-protobuf/pb.c
+++ build/lua-protobuf/pb.c
@@ -546,6 +546,9 @@ static int io_write(lua_State *L, FILE *f, int idx) {
 }
 
 static int Lio_read(lua_State *L) {
+#if defined(WINAPI_FAMILY_PARTITION)
+    return luaL_error(L, "unsupport api in uwp platform");
+#else
     const char *fname = luaL_optstring(L, 1, NULL);
     FILE *fp = stdin;
     int ret;
@@ -560,18 +563,26 @@ static int Lio_read(lua_State *L) {
     else (void)setmode(fileno(stdin), O_TEXT);
     if (ret != LUA_OK) { lua_pushnil(L); lua_insert(L, -2); return 2; }
     return 1;
+#endif
 }
 
 static int Lio_write(lua_State *L) {
+#if defined(WINAPI_FAMILY_PARTITION)
+    return luaL_error(L, "unsupport api in uwp platform");
+#else
     int res;
     (void)setmode(fileno(stdout), O_BINARY);
     res = io_write(L, stdout, 1);
     fflush(stdout);
     (void)setmode(fileno(stdout), O_TEXT);
     return res;
+#endif
 }
 
 static int Lio_dump(lua_State *L) {
+#if defined(WINAPI_FAMILY_PARTITION)
+    return luaL_error(L, "unsupport api in uwp platform");
+#else
     int res;
     const char *fname = luaL_checkstring(L, 1);
     FILE *fp = fopen(fname, "wb");
@@ -579,6 +590,7 @@ static int Lio_dump(lua_State *L) {
     res = io_write(L, fp, 2);
     fclose(fp);
     return res;
+#endif
 }
 
 LUALIB_API int luaopen_pb_io(lua_State *L) {
@@ -1212,6 +1224,9 @@ static int Lpb_load(lua_State *L) {
 }
 
 static int Lpb_loadfile(lua_State *L) {
+#if defined(WINAPI_FAMILY_PARTITION)
+    return luaL_error(L, "unsupport api in uwp platform");
+#else
     lpb_State *LS = lpb_lstate(L);
     const char *filename = luaL_checkstring(L, 1);
     size_t size;
@@ -1236,6 +1251,7 @@ static int Lpb_loadfile(lua_State *L) {
     lua_pushboolean(L, ret == PB_OK);
     lua_pushinteger(L, pb_pos(s)+1);
     return 2;
+#endif
 }
 
 static int lpb_pushtype(lua_State *L, const pb_Type *t) {
