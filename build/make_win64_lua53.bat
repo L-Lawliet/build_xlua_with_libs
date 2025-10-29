e8c3e3619d53d65dd25053ff3c17cfba9f465e4f
Lawliet <1577075604@qq.com>
2025-10-29 15:27:32 +0800

    【功能】【同步】调整Window的构建逻辑。调整发布逻辑。

--- build/make_win64_lua53.bat
+++ build/make_win64_lua53.bat
@@ -1,9 +1,24 @@
-mkdir build64 & pushd build64
-IF "%1" == "16" (
-    cmake -G "Visual Studio 16 2019" ..
-) ELSE (
-    cmake -G "Visual Studio 15 2017 Win64" ..
+
+set "__VS=Visual Studio 16 2019"
+set "__VSWhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+set "__VSDISPLAY="
+set "__VSVER="
+if exist "%__VSWhere%" (
+    for /f "tokens=*" %%p in (
+		'"%__VSWhere%" -latest -property catalog_productLineVersion'
+	) do set __VSDISPLAY=%%p
+
+	for /f "tokens=*" %%p in (
+		'"%__VSWhere%" -latest -property catalog_productDisplayVersion'
+	) do set __VSVER=%%p
+	
 )
+if "%__VSVER%" neq "" (	
+	set __VS=Visual Studio %__VSVER:~0,2% %__VSDisplay%
+)
+
+mkdir build64 & pushd build64
+cmake -G "%__VS%" -A x64 ..
 popd
 cmake --build build64 --config Release
 md plugin_lua53\Plugins\x86_64
