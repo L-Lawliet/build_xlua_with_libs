9f3b65312a40e87c9a6a496fa133156c12b55857
Lawliet <1577075604@qq.com>
2026-06-18 11:08:43 +0800

    【优化】修改为xcframework架构，兼容普通版本和模拟器版本

--- build/make_ios_simulator_lua53.sh
+++ build/make_ios_simulator_lua53.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -e
 
+# ===== 1. 真机 arm64 =====
+mkdir -p build_ios_device && cd build_ios_device
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
+cd ..
+cmake --build build_ios_device --config Release --parallel
+
+# ===== 2. 模拟器 arm64 =====
 mkdir -p build_ios_sim && cd build_ios_sim
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=SIMULATORARM64 -GXcode ../
 cd ..
 cmake --build build_ios_sim --config Release --parallel
 
+# ===== 3. 打包 XCFramework =====
 mkdir -p plugin_lua53_simulator/Plugins/iOS/
-cp build_ios_sim/Release-iphonesimulator/libxlua.a plugin_lua53_simulator/Plugins/iOS/libxlua.a
+xcodebuild -create-xcframework \
+    -library build_ios_device/Release-iphoneos/libxlua.a \
+    -library build_ios_sim/Release-iphonesimulator/libxlua.a \
+    -output plugin_lua53_simulator/Plugins/iOS/libxlua.xcframework
 
 echo ""
-echo "========== 构建完成 =========="
-echo "包含的架构:"
-lipo -info plugin_lua53_simulator/Plugins/iOS/libxlua.a
+echo "========== 打包完成 =========="
+ls plugin_lua53_simulator/Plugins/iOS/libxlua.xcframework/
