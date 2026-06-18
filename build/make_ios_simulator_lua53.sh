96646cc95cc721ba6c4a0fab5f0b2441dbdcb5af
Lawliet <1577075604@qq.com>
2026-06-18 09:16:23 +0800

    【修复】iOS模拟器版本剔除非模拟器架构

--- build/make_ios_simulator_lua53.sh
+++ build/make_ios_simulator_lua53.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 set -e
 
-# ===== 1. 真机 arm64 =====
-mkdir -p build_ios_device && cd build_ios_device
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
-cd ..
-cmake --build build_ios_device --config Release --parallel
-
-# ===== 2. Intel 模拟器 x86_64 =====
+# ===== 1. Intel 模拟器 x86_64 =====
 mkdir -p build_ios_sim_x86 && cd build_ios_sim_x86
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=SIMULATOR64 -GXcode ../
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=SIMULATOR64 -DARCHS=x86_64 -GXcode ../
 cd ..
 cmake --build build_ios_sim_x86 --config Release --parallel
 
-# ===== 3. Apple Silicon 模拟器 arm64 =====
+# ===== 2. Apple Silicon 模拟器 arm64 =====
 mkdir -p build_ios_sim_arm64 && cd build_ios_sim_arm64
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=SIMULATORARM64 -GXcode ../
 cd ..
 cmake --build build_ios_sim_arm64 --config Release --parallel
 
-# ===== 4. 合并三架构 =====
+# ===== 3. 合并双架构 =====
 mkdir -p plugin_lua53_simulator/Plugins/iOS/
 lipo -create \
-    build_ios_device/Release-iphoneos/libxlua.a \
     build_ios_sim_x86/Release-iphonesimulator/libxlua.a \
     build_ios_sim_arm64/Release-iphonesimulator/libxlua.a \
     -output plugin_lua53_simulator/Plugins/iOS/libxlua.a
